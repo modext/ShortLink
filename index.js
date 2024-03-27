14f0c2fd095be6955cca9cac4d7927bda9d7c959
modext <orjimodestus@gmail.com>
2024-03-28 00:27:04 +0100

    added cors

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import  express  from 'express';
 import routes from './src/routes/shortener.js'
+import cors from 'cors';
+
 
 const app = express();
 const port = process.env.PORT || 8000;
@@ -7,7 +9,9 @@ const port = process.env.PORT || 8000;
 app.use(express.json());
 
 app.use('/', routes);
-
+app.use(cors({
+  origin: 'http://localhost:3000' // Adjust this to match your frontend's origin
+}));
 app.get('/', (req, res) => {
   res.send('Welcome to ShortLink URL Shortening Service');
 });
